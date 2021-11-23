b717decc22d02cbd9967af8730d7224ad0586b48
Hun-X1 <younghun4218@gmail.com>
2021-11-23 10:56:20 +0900

    로고와 이미지 경로를 절대경로로 바꾸어서 이미지를 항상 로드하도록 수정

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -16,7 +16,7 @@ const Intro = () => {
 export default Intro;
 
 const MainImg = styled.div`
-    background-image: url(Intro/building2.png);
+    background-image: url(/Intro/building2.png);
     height: 500px;
     background-repeat: no-repeat;
     background-size: cover;
@@ -25,19 +25,31 @@ const MainImg = styled.div`
 
 const Welcome = () => {
     return (
-        <div>
-            <Title level={2}>"고객의 만족을 최우선으로 생각 합니다"</Title>
-            <Paragraph>
-                ㈜이룸인프라 홈페이지를 방문해 주셔서 감사합니다.
-            </Paragraph>
-            <Paragraph>
-                당사는 풍부한 경험과 기술력을 바탕으로 고객의 비즈니스 요구에
-                최적화된 IT인프라를 제공하고 있습니다.
-            </Paragraph>
-            <Paragraph>
-                도전과 열정으로 IT의 미래를 함께 하도록 노력하겠습니다.
-            </Paragraph>
-            <Paragraph>㈜이룸인프라 임직원 일동</Paragraph>
-        </div>
+        <FlexCenterDiv>
+            <div>
+                <Title style={{ textAlign: 'center' }} level={2}>
+                    "고객의 만족을 최우선으로 생각 합니다"
+                </Title>
+                <Paragraph>
+                    ㈜이룸인프라 홈페이지를 방문해 주셔서 감사합니다.
+                </Paragraph>
+                <Paragraph>
+                    당사는 풍부한 경험과 기술력을 바탕으로 고객의 비즈니스
+                    요구에 최적화된 IT인프라를 제공하고 있습니다.
+                </Paragraph>
+                <Paragraph>
+                    도전과 열정으로 IT의 미래를 함께 하도록 노력하겠습니다.
+                </Paragraph>
+                <Paragraph style={{ textAlign: 'right' }}>
+                    ㈜이룸인프라 임직원 일동
+                </Paragraph>
+            </div>
+        </FlexCenterDiv>
     );
 };
+const FlexCenterDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: left;
+`;
--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -14,7 +14,7 @@ const Nav = () => {
                 <Col>
                     <Link to='/'>
                         <img
-                            src='Logo.png'
+                            src='/Logo.png'
                             alt='이룸인프라 로고'
                             style={{ width: '100px' }}
                         />
@@ -32,28 +32,50 @@ const Nav = () => {
                                 <Link to='/introduction/welcome'>인사말</Link>
                             </Menu.Item>
                             <Menu.Item key='setting:2'>
-                                <Link to='/introduction/welcome'>사업영역</Link>
+                                <Link to='/introduction/businessArea'>
+                                    사업영역
+                                </Link>
                             </Menu.Item>
-                            <Menu.Item key='setting:3'>조직도</Menu.Item>
+                            <Menu.Item key='setting:3'>
+                                <Link to='introduction/organizational'>
+                                    조직도
+                                </Link>
+                            </Menu.Item>
                             <Menu.Item key='setting:5'>주요협력사</Menu.Item>
                             <Menu.Item key='setting:6'>찾아오시는 길</Menu.Item>
                         </SubMenu>
                         <SubMenu key='business' title='H/W 사업'>
                             <Menu.Item key='setting:7'>
-                                <Link to='hardware'>Server</Link>
+                                <Link to='hardware/server'>Server</Link>
                             </Menu.Item>
-                            <Menu.Item key='setting:8'>Storage</Menu.Item>
-                            <Menu.Item key='setting:9'>Workstation</Menu.Item>
-                            <Menu.Item key='setting:10'>PC</Menu.Item>
+                            <Menu.Item key='setting:8'>
+                                <Link to='hardware/storage'>Storage</Link>
+                            </Menu.Item>
+                            <Menu.Item key='setting:9'>
+                                <Link to='hardware/workstation'>
+                                    Workstation
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key='setting:10'>
+                                <Link to='hardware/pc'>PC</Link>
+                            </Menu.Item>
                         </SubMenu>
                         <SubMenu key='solution' title='솔루션 사업'>
-                            <Menu.Item key='setting:11'>Nutanix</Menu.Item>
-                            <Menu.Item key='setting:12'>Vmware</Menu.Item>
+                            <Menu.Item key='setting:11'>
+                                <Link to='solution/Nutanix'>Nutanix</Link>
+                            </Menu.Item>
+                            <Menu.Item key='setting:12'>
+                                <Link to='solution/Vmware'>Vmware</Link>
+                            </Menu.Item>
                         </SubMenu>
 
                         <SubMenu key='service' title='서비스 사업'>
-                            <Menu.Item key='setting:13'>IT유지보수</Menu.Item>
-                            <Menu.Item key='setting:14'>IT장비이전</Menu.Item>
+                            <Menu.Item key='setting:13'>
+                                <Link to='service/maintenance'>IT유지보수</Link>
+                            </Menu.Item>
+                            <Menu.Item key='setting:14'>
+                                <Link to='service/transfer'>IT장비이전</Link>
+                            </Menu.Item>
                         </SubMenu>
                     </Menu>
                 </Col>
