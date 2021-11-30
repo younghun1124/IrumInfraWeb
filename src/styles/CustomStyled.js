60a0c050ae9fd366309b09cdfef993e025cc55cf
Hun-X1 <younghun4218@gmail.com>
2021-11-30 11:41:00 +0900

    좌우여백을 띄우는 InnerContainer Div

---
+++ src/styles/CustomStyled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import theme from './theme';
+
+export const InnerContainer = styled.div`
+    ${theme.common.flexCenter}
+    max-width: 1140px;
+    margin: auto;
+`;
+
+// const CustomStyled = {
+//     InnerContainer,
+// };
+// export default CustomStyled;
