6916081b2d5621e3a4710b4f40d79f26517a48c8
Hun-X1 <younghun4218@gmail.com>
2021-11-30 17:28:13 +0900

    css 수정

--- src/components/BusinessArea.js
+++ src/components/BusinessArea.js
@@ -9,35 +9,39 @@ const { Title, Paragraph } = Typography;
 
 function BusinessArea() {
     return (
-        <Row gutter={16} justify='space-between'>
-            <Fade bottom cascade>
-                <Col css={FlexCenter} span={8}>
+        <Row
+            justify='space-between'
+            css={css`
+                width: 100%;
+            `}
+        >
+            <Fade cascade>
+                <Col css={FlexCenter}>
                     <Title level={4}>하드웨어 사업</Title>
                     <StyledImg src='/hw.jpg' alt='하드웨어 사업' />
-                    <Paragraph>
-                        강력한 성능의 Rack & Tower Server 관리가 용이하고 확장이
-                        간편한 Storage 압도적 성능과 내구성의 Workstation
-                    </Paragraph>
+                    <Paragraph>강력한 성능의 Rack & Tower Server</Paragraph>
+                    <Paragraph>관리가 용이하고 확장이 간편한 Storage</Paragraph>
+                    <Paragraph>압도적 성능과 내구성의 Workstation</Paragraph>
                 </Col>
             </Fade>
-            <Fade bottom cascade>
-                <Col css={FlexCenter} span={8}>
+            <Fade cascade>
+                <Col css={FlexCenter}>
                     <Title level={4}>솔루션 사업</Title>
                     <StyledImg src='/sol2.png' alt='하드웨어 사업' />
                     <Paragraph>
-                        강력한 성능의 모바일 워크스테이션 ThinkPad 압도적인
-                        성능과 내구성을 지닌 ThinkStation
+                        강력한 성능의 모바일 워크스테이션 ThinkPad
+                    </Paragraph>
+                    <Paragraph>
+                        압도적인 성능과 내구성을 지닌 ThinkStation
                     </Paragraph>
                 </Col>
             </Fade>
-            <Fade bottom cascade>
-                <Col css={FlexCenter} span={8}>
+            <Fade cascade>
+                <Col css={FlexCenter}>
                     <Title level={4}>Contact us</Title>
                     <StyledImg src='/call center.png' alt='하드웨어 사업' />
-                    <Paragraph>
-                        TEL. 02-8577-008 FAX. 02-8577-009 E-mail.
-                        mhjung@iruminfra.co.kr
-                    </Paragraph>
+                    <Paragraph>TEL. 02-8577-008 FAX. 02-8577-009</Paragraph>
+                    <Paragraph>E-mail. mhjung@iruminfra.co.kr</Paragraph>
                 </Col>
             </Fade>
         </Row>
