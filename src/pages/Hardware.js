0993adb62891a392ea91a210092084a4433e4017
Hun-X1 <younghun4218@gmail.com>
2021-12-01 10:16:31 +0900

    상품 목록 을 생성하는 코드 리팩터링(외부파일로 분리)

--- src/pages/Hardware.js
+++ src/pages/Hardware.js
@@ -1,35 +1,30 @@
-import React, { useState, useRef } from 'react';
-
-import styled from '@emotion/styled';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Typography } from 'antd';
-import Iframe from 'react-iframe';
+import { InnerContainer } from '../styles/CustomStyled';
+import ModelList from '../components/ModelList';
+
 const { Title, Text, Paragraph } = Typography;
 const Hardware = () => {
     return (
         <>
-            <Routes>
-                {/* <Route path='server' element={<ServerList />} /> */}
-                {/* <Route path='storage' element={< />} />
-                <Route path='workstation' element={< />} />
-                <Route path='pc' element={< />} /> */}
-            </Routes>
+            <InnerContainer>
+                <div style={{ width: '100%' }}>
+                    <Routes>
+                        <Route
+                            path='server'
+                            element={
+                                <ModelList jsonFileName={'serverList.json'} />
+                            }
+                        />
+                        {/* <Route path='storage' element={< />} />
+                        <Route path='workstation' element={< />} />
+                        <Route path='pc' element={< />} /> */}
+                    </Routes>
+                </div>
+            </InnerContainer>
         </>
     );
 };
 
 export default Hardware;
-//TODO: json파일을 axios로 요청하는 코드 작성
-
-// const ServerList = () => {
-//     return (
-//         <ol>
-//             {serverJson.models.map((model) => (
-//                 <li>
-//                     <img src={model.imgLink} alt={model.name} />
-//                     <h3>{model.name}</h3>
-//                 </li>
-//             ))}
-//         </ol>
-//     );
-// };
