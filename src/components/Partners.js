316e11fdb4f0e4e439078445233232f4d39a0f96
Hun-X1 <younghun4218@gmail.com>
2021-12-14 10:53:36 +0900

    #3 이슈해결

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -6,58 +6,52 @@ import styled from '@emotion/styled';
 import Zoom from 'react-reveal/Zoom';
 const Partners = () => {
     return (
-        <Zoom cascade>
-            <GridDiv>
-                <Partner
-                    imgLink='/Intro/Partners/Lenovo_logo_red.png'
-                    hyperLink='https://www.lenovo.com/kr/ko/'
-                >
-                    Lenovo
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/HPE-logo-exence.png'
-                    hyperLink='http://www.hpe.com/'
-                >
-                    HP Enterprise
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/dell-logo.png'
-                    hyperLink='http://www.dell.com/'
-                >
-                    Dell
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/LGe.jpg'
-                    hyperLink='http://www.lge.co.kr/'
-                >
-                    LG 전자
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/MS.png'
-                    hyperLink='http://www.microsoft.com//'
-                >
-                    Microsoft
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/nutanix.png'
-                    hyperLink='https://www.nutanix.com/kr'
-                >
-                    Nutanix
-                </Partner>
-                <Partner
-                    imgLink='/Intro/Partners/vmware.png'
-                    hyperLink='http://www.vmware.com/'
-                >
-                    vmware
-                </Partner>
-                {/* <Partner
-                    imgLink='/Intro/Partners/Tidalscale.png'
-                    hyperLink='http://www.tidalscale.com/'
-                >
-                    Tidalscale
-                </Partner> */}
-            </GridDiv>
-        </Zoom>
+        // <Zoom cascade>
+        <GridDiv>
+            <Partner
+                imgLink='/Intro/Partners/Lenovo_logo_red.png'
+                hyperLink='https://www.lenovo.com/kr/ko/'
+            >
+                Lenovo
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/HPE-logo-exence.png'
+                hyperLink='http://www.hpe.com/'
+            >
+                HP Enterprise
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/dell-logo.png'
+                hyperLink='http://www.dell.com/'
+            >
+                Dell
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/LGe.jpg'
+                hyperLink='http://www.lge.co.kr/'
+            >
+                LG 전자
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/MS.png'
+                hyperLink='http://www.microsoft.com//'
+            >
+                Microsoft
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/nutanix.png'
+                hyperLink='https://www.nutanix.com/kr'
+            >
+                Nutanix
+            </Partner>
+            <Partner
+                imgLink='/Intro/Partners/vmware.png'
+                hyperLink='http://www.vmware.com/'
+            >
+                vmware
+            </Partner>
+        </GridDiv>
+        // {/* </Zoom> */}
     );
 };
 
@@ -79,12 +73,12 @@ const LogoImg = styled.img`
 `;
 
 const GridDiv = styled.div`
-    margin-top: 50px;
-    margin-bottom: 50px;
+    padding-top: 50px;
+    padding-bottom: 50px;
     display: grid;
 
     grid-template-columns: repeat(auto-fill, minmax(20%, auto));
-    gap: 5%;
+    /* gap: 5%; */
     ${({ theme }) => theme.device.tablet} {
         grid-template-columns: repeat(auto-fill, minmax(50%, auto));
     }
@@ -97,6 +91,7 @@ const GridItem = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+
     padding: 2rem;
     transition: all 0.1s linear;
     &:hover {
